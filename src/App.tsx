e050bc1369ab4a5d4ba9076ab072ac84a9bf8633
Imrie83 <zielinskimar83@gmail.com>
2025-12-19 20:40:29 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>React + TypeScript + Vite</h1>
+      <p>Start building your application here!</p>
+    </div>
+  );
+}
+
+export default App;
