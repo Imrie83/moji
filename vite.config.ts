b0401949be46c8f6229f6306191d33a9e431daa3
Imrie83 <zielinskimar83@gmail.com>
2025-12-28 20:00:45 +0100

    build optimisation

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/moji/',
-})
+    plugins: [react()],
+    base: '/moji/',
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    'react-vendor': ['react', 'react-dom', 'react-bootstrap', 'bootstrap'],
+                    'mui-vendor': ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
+                    'animation-vendor': ['framer-motion', 'tsparticles', '@tsparticles/react', '@tsparticles/slim'],
+                    'utils-vendor': ['wanakana', 'zustand'],
+                },
+            },
+        },
+    },
+});
